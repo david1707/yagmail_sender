5ba50a34bc5930781ea3034b22b2c2d0b5a14a1e
david1707 <DavidAlacant@gmail.com>
2019-09-22 17:39:29 +0200

    Readme + small fixes on the code

--- yagmail_sender.py
+++ yagmail_sender.py
@@ -2,23 +2,22 @@ import yagmail
 
 
 sender_email = 'smtpforletslearnabout@gmail.com'
-receiver_email = 'smtpforletslearnabout@gmail.com'
+receiver_emails = ['smtpforletslearnabout@gmail.com', 'smtpforletslearnabout@gmail.com', 'smtpforletslearnabout@gmail.com']
 subject = "Check THIS out"
 sender_password = input(f'Please, enter the password for {sender_email}:\n')
 
-yag = yagmail.SMTP(user='smtpforletslearnabout@gmail.com', password=sender_password)
+try: 
+  yag = yagmail.SMTP(user='smtpforletslearnabout@gmail.com', password=sender_password)
 
-contents = [
-  "This is the first paragraph in our email",
-  "As you can see, we can send a list of strings,",
-  "being this our third one",
-  "BTW, here you have two pictures of dogs and cats. You need them:\n",
-  "C:\\Users\\Void\\Desktop\\Codi\\Python\\yagmail\\dogs.jpg",
-  "C:\\Users\\Void\\Desktop\\Codi\\Python\\yagmail\\cats.jpg",
-]
+  contents = [
+    "This is the first paragraph in our email",
+    "As you can see, we can send a list of strings,",
+    "being this our third one",
+    "C:\\Users\\Void\\Desktop\\Codi\\Python\\yagmail\\dodgs.jpg",
+    "C:\\Users\\Void\\Desktop\\Codi\\Python\\yagmail\\cats.jpg"
+  ]
 
-# yag.send(sender_email, subject, contents)
+  yag.send(receiver_emails, subject, contents)
 
-# yag.send([sender_email, sender_email, sender_email, sender_email], subject, contents)
-
-# yag.send([sender_email, sender_email, sender_email, sender_email], subject, contents, ["C:\\Users\\Void\\Desktop\\Codi\\Python\\yagmail\\dogs.jpg", "C:\\Users\\Void\\Desktop\\Codi\\Python\\yagmail\\cats.jpg"])
+except Exception as e:
+  print(f'Something went wrong!\n{e}')
